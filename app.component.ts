e62dcaeb0d37722950084601dcaa0db57b192e05
Harmanjit Singh <57390891+harman20@users.noreply.github.com>
2020-09-03 15:03:40 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  constructor() { }
+  title = 'CRUD operations in angular';
+
+  ngOnInit() {
+  }
+}
